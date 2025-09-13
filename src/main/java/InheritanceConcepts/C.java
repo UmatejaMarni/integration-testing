3fac67a5b0a6c109a506d1b20e2620230b41da5c
Umateja Marni <umateja.marni@gmail.com>
2025-09-13 18:37:04 +0530

    added new classes

---
+++ src/main/java/InheritanceConcepts/C.java
@@ -0,0 +1,9 @@
+package InheritanceConcepts;
+
+public class C extends A{
+
+
+
+    //public class C extends A,B --- multiple inheritance not possible in java,
+    //multiple inheritance can be achieved via interfaces
+}
