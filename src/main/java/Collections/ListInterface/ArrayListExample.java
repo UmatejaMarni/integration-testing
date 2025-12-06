1fb7be2ba534cb532942652ee43a54af37ead9fd
UmatejaMarni <82547744+UmatejaMarni@users.noreply.github.com>
2025-12-06 21:19:53 +0530

    Feature/udemy (#5)
    * added new files
    
    * added new classes
    
    * added new classes
    
    * added new files
    
    * added the new files
    
    * added new classes
    
    * added new files
    
    * collections

---
+++ src/main/java/Collections/ListInterface/ArrayListExample.java
@@ -0,0 +1,38 @@
+package Collections.ListInterface;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args){
+
+        ArrayList<String> a = new ArrayList<String> ();
+        a.add("uma");
+        a.add("teja");
+        a.add("professional");
+        a.add("employee");
+        System.out.println(a);
+        System.out.println(a.indexOf("employee"));
+        System.out.println(a.get(2));
+        a.remove("teja");
+        System.out.println(a);
+        System.out.println(a.size());
+        System.out.println (a.isEmpty());
+        System.out.println(a.contains("teja"));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/Collections/SetInterface/HashSetExample.java
@@ -0,0 +1,34 @@
+package Collections.SetInterface;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetExample {
+
+
+    public static void main(String[] args){
+
+        HashSet<String> hs = new HashSet<String>();
+        hs.add("he");
+        hs.add("she");
+        hs.add("uma");
+        hs.add("teja");
+        hs.add("good girl");
+        System.out.println(hs);
+        hs.remove("he");
+        System.out.println(hs);
+        System.out.println(hs.size());
+        System.out.println(hs.isEmpty());
+        System.out.println(hs.contains("uma"));
+
+           Iterator<String> i = hs.iterator();
+           while(i.hasNext()){
+
+               System.out.println(i.next());
+
+           }
+
+
+    }
+
+}
