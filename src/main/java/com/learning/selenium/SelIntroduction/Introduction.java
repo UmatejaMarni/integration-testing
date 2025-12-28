f0971669363a42329ca56f614ddd7c2085ab18de
Umateja Marni <umateja.marni@gmail.com>
2025-12-28 23:53:25 +0530

    updated

--- src/main/java/com/learning/selenium/SelIntroduction/Introduction.java
+++ src/main/java/com/learning/selenium/SelIntroduction/Introduction.java
@@ -1,19 +1,34 @@
 package com.learning.selenium.SelIntroduction;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Introduction {
 
 public static void main(String[] args){
 
+
     WebDriver driver = new ChromeDriver();
+    driver.manage().window().setSize(new Dimension(1920,1080)); // worked in asus also
     driver.get("https://rahulshettyacademy.com/");
-    driver.manage().window().maximize();
+
+
+    //chromeoptions worked in my asus laptop also to maximize chrome
+    /*ChromeOptions options = new ChromeOptions();
+    options.addArguments("start-maximized");
+    WebDriver driver = new ChromeDriver(options);
+    driver.get("https://rahulshettyacademy.com/");*/
+
+    //driver.manage().window().maximize(); //this didnt work in asus laptop
+
+
+
     System.out.println(driver.getTitle());
     System.out.println(driver.getCurrentUrl());
-    //driver.close();
-    driver.quit();
+    //driver.close(); //cloesed the current windows
+    driver.quit(); //closed all windows
 
 
 
